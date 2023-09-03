29d6b3358881049ea62f1b8e552282765238952d
Droid-M <62139366+Droid-M@users.noreply.github.com>
2023-09-03 00:46:38 -0300

    update Database.php and Seedable.php

--- php/services/Database.php
+++ php/services/Database.php
@@ -2,15 +2,20 @@
 
 namespace php\services;
 
+use Closure;
+use Exception;
 use PDO;
 use PDOException;
 use php\services\Response;
+use php\traits\Seedable;
 
 /** @var null|PDO $pdo19293s8389210283383801 */
 $pdo19293s8389210283383801 = null;
 
 class Database
 {
+    use Seedable;
+
     public static function init()
     {
         try {
@@ -32,5 +37,43 @@ class Database
     {
         global $pdo19293s8389210283383801;
         return $pdo19293s8389210283383801;
+    }
+
+    public static function beginTransaction()
+    {
+        global $pdo19293s8389210283383801;
+        $pdo19293s8389210283383801->beginTransaction();
+    }
+
+    public static function commit()
+    {
+        global $pdo19293s8389210283383801;
+        $pdo19293s8389210283383801->commit();
+    }
+
+    public static function rollback()
+    {
+        global $pdo19293s8389210283383801;
+        $pdo19293s8389210283383801->rollBack();
+    }
+
+    public static function closeConnection()
+    {
+        global $pdo19293s8389210283383801;
+        $pdo19293s8389210283383801 = null;
+    }
+
+    public static function transaction(Closure $callback)
+    {
+        global $pdo19293s8389210283383801;
+        try {
+            $pdo19293s8389210283383801->beginTransaction();
+            $toReturn = $callback();
+            $pdo19293s8389210283383801->commit();
+            return $toReturn;
+        } catch (Exception $e) {
+            $pdo19293s8389210283383801->rollBack();
+            return throw $e;
+        }
     }
 }
--- php/traits/Seedable.php
+++ php/traits/Seedable.php
@@ -1,16 +1,21 @@
 <?php
 
+namespace php\traits;
+
+use PDOException;
+
 trait Seedable
 {
     // Function to generate a random value for the ENUM field 'payment_method'
-    private function randomPaymentMethod()
+    private static function randomPaymentMethod()
     {
         $paymentMethods = ['pix', 'credit_card', 'cash'];
         return $paymentMethods[array_rand($paymentMethods)];
     }
 
-    protected function seedTables()
+    public static function seedTables()
     {
+        $pdo = static::getPDO();
         try {
             // Generate and insert data into the 'cashiers' tables
             for ($i = 1; $i <= 10; $i++) {
@@ -18,12 +23,12 @@ trait Seedable
                 $is_blocked = rand(0, 1);
 
                 $sql = "INSERT INTO cashiers (ip, is_blocked) VALUES (:ip, :is_blocked)";
-                $stmt = $this->db->prepare($sql);
+                $stmt = $pdo->prepare($sql);
                 $stmt->bindParam(':ip', $ip);
                 $stmt->bindParam(':is_blocked', $is_blocked);
                 $stmt->execute();
 
-                $cashierId = $this->db->lastInsertId(); // Gets the ID of the cashier inserted
+                $cashierId = $pdo->lastInsertId(); // Gets the ID of the cashier inserted
 
                 // Generate and insert cashier-related data in the 'purchases' table
                 for ($j = 1; $j <= 20; $j++) { // 20 compras por caixa
@@ -31,11 +36,11 @@ trait Seedable
                     $paid = rand(0, 1);
                     $purchaser_name = "Customer " . $i . "-" . $j;
                     $purchaser_cpf = str_pad(rand(1, 99999999999), 11, "0", STR_PAD_LEFT);
-                    $payment_method = $this->randomPaymentMethod();
+                    $payment_method = self::randomPaymentMethod();
 
                     $sql = "INSERT INTO purchases (total_value, paid, origin_cashier, purchaser_name, purchaser_cpf, payment_method) 
                             VALUES (:total_value, :paid, :origin_cashier, :purchaser_name, :purchaser_cpf, :payment_method)";
-                    $stmt = $this->db->prepare($sql);
+                    $stmt = $pdo->prepare($sql);
                     $stmt->bindParam(':total_value', $total_value);
                     $stmt->bindParam(':paid', $paid);
                     $stmt->bindParam(':origin_cashier', $cashierId);
@@ -53,20 +58,20 @@ trait Seedable
                 $bar_code = "BAR" . str_pad($i, 5, "0", STR_PAD_LEFT);
 
                 $sql = "INSERT INTO products (name, stock_quantity, bar_code) VALUES (:name, :stock_quantity, :bar_code)";
-                $stmt = $this->db->prepare($sql);
+                $stmt = $pdo->prepare($sql);
                 $stmt->bindParam(':name', $name);
                 $stmt->bindParam(':stock_quantity', $stock_quantity);
                 $stmt->bindParam(':bar_code', $bar_code);
                 $stmt->execute();
 
-                $productId = $this->db->lastInsertId();
+                $productId = $pdo->lastInsertId();
 
                 // Generate and insert data into the 'purchase_product' table (many-to-many relationship)
                 for ($j = 1; $j <= 5; $j++) { // 5 products per purchase
                     $purchaseId = rand(1, 40);
 
                     $sql = "INSERT INTO purchase_product (product_id, purchase_id) VALUES (:product_id, :purchase_id)";
-                    $stmt = $this->db->prepare($sql);
+                    $stmt = $pdo->prepare($sql);
                     $stmt->bindParam(':product_id', $productId);
                     $stmt->bindParam(':purchase_id', $purchaseId);
                     $stmt->execute();
